491597480a415ed9e67e21b901a0d6ed4bfe5df6
Joel Parker Henderson <joel@joelparkerhenderson.com>
2012-09-24 10:37:19 -0700

    Run rails new sampler --database sqlite3 --skip-bundle --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sampler::Application.config.secret_token = '2afff664493038e32532e29b1003eac9b7dd847acae616116a69a6904ea57d17969758fa956bcb8b7f62f94730a0fed222191951fac4861b2d25a1a981bafb34'
